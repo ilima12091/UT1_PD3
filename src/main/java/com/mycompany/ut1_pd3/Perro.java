a0e3e9717eaa7a4f182cba85b40e6e3cae10b567
Ignacio Lima <limaignacio12091@gmail.com>
2022-03-26 16:37:38 -0300

    Agrego la implementación de la interfaz Mamifero a Perro

--- src/main/java/com/mycompany/ut1_pd3/Perro.java
+++ src/main/java/com/mycompany/ut1_pd3/Perro.java
@@ -8,22 +8,40 @@ package com.mycompany.ut1_pd3;
  *
  * @author Ignacio
  */
-public class Perro {
+public class Perro implements Mamifero {
+
     private String nombre;
-    
+
     public Perro(String nombre) {
         this.nombre = nombre;
     }
-    
+
+    @Override
     public String getNombre() {
         return this.nombre;
     }
-    
+
     public void ladrar() {
         System.out.println("wof");
     }
-    
+
+    @Override
     public boolean respondeA(String unNombre) {
         return this.nombre.equals(unNombre);
+    }
+
+    @Override
+    public void caminar() {
+        System.out.println(this.nombre + " está caminando");
+    }
+
+    @Override
+    public void correr() {
+        System.out.println(this.nombre + " está corriendo");
+    }
+
+    @Override
+    public void saltar() {
+        System.out.println(this.nombre + " está saltando");
     }
 }
